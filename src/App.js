4a91958eaaa7cda38c098812a29cb2bc3a226d1f
regbake <regbake@gmail.com>
2017-10-20 07:51:37 -0700

    nav bar progress

--- src/App.js
+++ src/App.js
@@ -12,24 +12,31 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>This is the app component</h1>
-        <ul>
-          <li>wassup</li>
-        </ul>
-
       <Router>
-      <div>
-        <nav>
-          <Link to = "/"> Go to Home </Link> {' '}
-          <Link to = "/cart"> Go to cart </Link> {' '}
-        </nav>
-        <Route exact path = "/" component = {Pumpkin} />
-        <Route path = "/cart" component = {Cart} />
-      </div>
-      </Router>
+        <div>
+          <div>
+          <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+              <a class="navbar-brand" href="#">Pumpkins Direct</a>
 
-    </div>
+              <form className="navbar-form navbar-left" role="search">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search" />
+                </div>
+                <button type="submit" className="btn btn-default">Submit!</button>
+              </form>
+              <Link to = "/"> Go to Home </Link> {' '}
+              <Link to = "/cart"> Go to cart </Link> {' '}
+
+              <p class="navbar-text">Signed in as Pumpkin McGee</p>
+            </div>
+          </nav>
+          </div>
+          <Route exact path = "/" component = {Pumpkin} />
+          <Route path = "/cart" component = {Cart} />
+
+        </div>
+      </Router>
     );
   }
 }
